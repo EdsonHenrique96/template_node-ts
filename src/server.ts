b6349006003251a6b1368deb242523789f3775f0
Edson Batista <henrique.ti1196@gmail.com>
2020-06-02 14:26:50 -0300

    boilerplate node+ts+eslint+prettier

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+const api = express();
+
+api.use(routes);
+
+api.listen(3333, () => {
+  console.log('🚀 Server started on port 3333');
+});
